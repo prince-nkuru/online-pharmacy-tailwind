b2d6ac0ebaeaeaf6355cbb5679c48492fa2aa689
prince-nkuru <nkuruprinces@gmail.com>
2024-10-12 14:20:56 +0530

    added pro1

---
+++ src/components/Pro1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export  const datas = [
+  { id: 1, name: 'Paracetamol', price: '$19.99', img: 'https://www.chemist-4-u.com/media/catalog/product/p/a/paracetamol_tablets_500mg.jpg' },
+  { id: 2, name: 'morphine', price: '$29.99', img: 'https://tse3.mm.bing.net/th?id=OIP.oEmKeunkkJtHULOm6U8gpgHaE8&pid=Api&P=0&h=220' },
+  { id: 3, name: 'codeine', price: '$39.99', img: 'https://tse3.mm.bing.net/th?id=OIP.p54j2Jq0bRi6iGb0_mBH4AHaE8&pid=Api&P=0&h=220' },
+  { id: 4, name: 'augmentin', price: '$49.99', img: 'https://tse4.mm.bing.net/th?id=OIP.TYk7F8ttOE0XODGcJz26TwHaEX&pid=Api&P=0&h=220' },
+  { id: 5, name: 'Product 1', price: '$19.99', img: 'https://tse4.mm.bing.net/th?id=OIP.NsiespZAeMItSp_itm2_oAHaHa&pid=Api&P=0&h=220' },
+  { id: 6, name: 'Product 2', price: '$29.99', img: 'https://tse2.mm.bing.net/th?id=OIP.jo4zMXllm4VwZTpwO3YDDgHaHa&pid=Api&P=0&h=220' },
+  { id: 7, name: 'Product 3', price: '$39.99', img: 'https://tse3.mm.bing.net/th?id=OIP.p54j2Jq0bRi6iGb0_mBH4AHaE8&pid=Api&P=0&h=220' },
+  { id: 8, name: 'Product 4', price: '$49.99', img: 'https://tse4.mm.bing.net/th?id=OIP.TYk7F8ttOE0XODGcJz26TwHaEX&pid=Api&P=0&h=220' },
+];
