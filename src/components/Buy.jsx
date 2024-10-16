30a6b3802457d2add926f0a3bc558a61a9ce00d0
prince-nkuru <nkuruprinces@gmail.com>
2024-10-16 15:36:08 +0530

    added buy

--- src/components/Buy.jsx
+++ src/components/Buy.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import { datas } from "./Pro1";
+import Button from "./Button";
 
 function Buy() {
   const navigate = useNavigate();
@@ -10,7 +11,7 @@ function Buy() {
   };
 
   return (
-    <section id="products" className="p-5">
+    <section id="products" className="p-5 mt-12">
       <h2 className="text-2xl font-bold mb-4">Featured Products</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {datas.map((product) => (
@@ -21,16 +22,17 @@ function Buy() {
             <img
               src={product.img}
               alt={product.name}
-              className="max-w-full h-auto mb-3 rounded-2xl"
+              className="w-48 h-48 object-cover mb-3 rounded-2xl"
             />
             <h3 className="text-lg font-semibold">{product.name}</h3>
             <p className="text-md text-gray-700">{product.price}</p>
-            <button
+            {/* <button
               onClick={handleCart}
               className="mt-2 bg-orange-500 text-black py-2 px-4 rounded hover:bg-orange-600 focus:outline-none"
             >
               Add to Cart
-            </button>
+            </button> */}
+            <Button onClick={handleCart}>Add to cart</Button>
           </div>
         ))}
       </div>
