a14a1c42ff7988cc9d0cb9239a2091163cfdba9d
prince-nkuru <nkuruprinces@gmail.com>
2024-10-23 17:04:26 +0530

    added app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,33 @@ import Hea from "./components/Hea";
 import Body from "./components/Body";
 import Phone from "./components/PhoneContact";
 import Bout from "./components/BoutUs";
-import InLog from "./components/InLog";
+import Login from "./components/Login";
 import Buy from "./components/Buy";
 import MedicineDetail from "./components/MedicineDetail"; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from './components/AuthContext';
+import Cart from "./components/Cart";
+import { CartProvider } from "./components/CartContext"; // Import CartProvider
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider> {/* Wrap the whole app with AuthProvider */}
+      <CartProvider> 
         <Hea />
 
         <Routes>
+        <Route path="/cart" element={<Cart />} />
           <Route path="/body" element={<Body />}></Route>
           <Route path="/phone" element={<Phone />}></Route>
           <Route path="/bout" element={<Bout />}></Route>
-          <Route path="inlog" element={<InLog />}></Route>
-          <Route path="buy" element={<Buy />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/buy" element={<Buy />}></Route>
           <Route path="/medicine/:id" element={<MedicineDetail />} />
         </Routes>
-      </BrowserRouter>
-    </>
+        </CartProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
