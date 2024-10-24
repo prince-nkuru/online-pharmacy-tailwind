251532d58c9e5ccc2081562642aa4ca49f02df60
prince-nkuru <nkuruprinces@gmail.com>
2024-10-24 16:58:01 +0530

    added boutus

--- src/components/BoutUs.jsx
+++ src/components/BoutUs.jsx
@@ -3,11 +3,11 @@ import team from '../assets/team.jpg'
 
 function BoutUs() {
   return (
-    <section className=' w-screen bg-cover bg-center min-h-screen mt-12' 
+    <section className=' w-screen bg-cover bg-center min-h-screen mt-24' 
     style={{backgroundImage: `url(${team}) `}}>
-      <div className="mt-2 ml-10 mr-10  bg-green-600 w-1/4 p-6">
-        <h2 className='font-bold text-2xl m-3'>About Us</h2>
-        <p className="m-5">
+      <div className="mt-2 ml-10 mr-10  bg-slate-800 w-1/4 p-6">
+        <h2 className='font-bold text-2xl m-3 text-white'>About Us</h2>
+        <p className="m-5 text-white">
           Welcome to IBOZA Pharmacy, your trusted online destination for
           affordable ,high-quality healthcare products.Founded in 2024, we are
           committed to providing easy access to prescription medications,
@@ -16,23 +16,23 @@ function BoutUs() {
           with reliable healthcare solutions while prioritizing safety,
           convenience, and customer satisfaction
         </p>
-        <h2 className='font-bold text-2xl m-3'>Licensed & Accredited</h2>
-        <p className='m-5'>
+        <h2 className='font-bold text-2xl m-3 text-white'>Licensed & Accredited</h2>
+        <p className='m-5 text-white'>
           IBOZA Pharmacy is a fully licensed and accredited online pharmacy. We
           comply with all federal and state regulations to ensure the safety and
           legitimacy of our products. our pharmacy is certified by Rwanda FDA
           and follows stringent quality control standards to meet the
           expectation of our customers
         </p>
-        <h2 className='font-bold text-2xl m-3'>Products & services</h2>
-        <ul>
+        <h2 className='font-bold text-2xl m-3 text-white'>Products & services</h2>
+        <ol className='text-red-800'>
           <li>Prescription medications</li>
           <li>over-the-counter treatments</li>
           <li>vitamins and health supplement</li>
           <li>Medical devices and equipment</li>
-        </ul>
-        <h2 className='font-bold text-2xl m-3'>Expert Pharmacist Team</h2>
-        <p>
+        </ol>
+        <h2 className='font-bold text-2xl m-3 text-white'>Expert Pharmacist Team</h2>
+        <p className='text-white'>
           Our team of licensed and experienced pharmacists is always available
           to answer your questions and provide expert adviceon medication use,
           side effects, and drug interactions. with a lot experience, our
