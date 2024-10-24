8da10c34e8449ef57c17f8afba39aa05a804e47a
prince-nkuru <nkuruprinces@gmail.com>
2024-10-24 16:56:59 +0530

    added body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router";
-import pexel from '../assets/pexels-tima-miroshnichenko-7034125.jpg';
-import SearchEngine from './SearchEngine';
-import Button from './Button';
+import pexel from "../assets/pexels-tima-miroshnichenko-7034125.jpg";
+// import SearchEngine from "./SearchEngine";
+import Button from "./Button";
 
 function Body() {
   const nav = useNavigate();
 
   const handleShop = () => {
-    nav('/buy');
+    nav("/buy");
   };
 
   return (
     <section
-      className="flex flex-col items-center bg-cover bg-center h-screen mt-10"
+      className="flex flex-col items-center bg-cover bg-center h-screen mt-28"
       style={{ backgroundImage: `url(${pexel})` }}
     >
-      <h1 className="text-white text-4xl font-bold mb-10 mt-10">Welcome to IBOZA Pharmacy</h1>
-      <h2 className="text-black text-2xl mb-10 font-bold">Your Health, Our Priority</h2>
-      <p className="text-black mb-6 text-xl">Browse a wide range of medications and health products.</p>
-      {/* <a
-        href="#products"
-        className="btn bg-white text-black py-2 px-4 rounded hover:text-teal-300"
-        onClick={handleShop}
-      >
-        Shop Now
-      </a> */}
+      <h1 className="text-white text-4xl font-bold mb-10 mt-10">
+        Welcome to <span className="text-teal-300 font-bold">IBOZA </span>Pharmacy
+      </h1>
+      <h2 className="text-black text-2xl mb-10 font-bold">
+        Your Health, Our Priority
+      </h2>
+      <p className="text-black mb-6 text-xl">
+        Browse a wide range of medications and health products.
+      </p>
+
       <Button onClick={handleShop}>Shop Now</Button>
-      <SearchEngine />
+      
     </section>
   );
 }
